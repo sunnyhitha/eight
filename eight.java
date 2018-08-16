4508c40e9c2034d0178da474c064e22dd66cff64
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-16 12:27:11 +0530

    Create eight.java

---
+++ eight.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		float f;
+		Scanner sc=new Scanner(System.in);
+		f=sc.nextFloat();
+		int a=Math.round(f);
+		a=a+1;
+		System.out.print(a);// your code goes here
+	}
+}
